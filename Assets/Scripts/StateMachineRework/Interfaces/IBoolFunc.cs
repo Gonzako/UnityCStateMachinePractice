3d234fc42c8eb4605acbe0dbd95c8c6629f117bb
Gonzako <al341522@uji.es>
2020-03-26 19:48:32 +0100

    Added Iboolfunc for a CP bool command pattern

---
+++ Assets/Scripts/StateMachineRework/Interfaces/IBoolFunc.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IBoolFunc
+{
+    bool evaluate(float val1, float val2, params string[] input);
+}
--- Assets/Scripts/StateMachineRework/Interfaces/IState.cs
+++ Assets/Scripts/StateMachineRework/Interfaces/IState.cs
@@ -6,10 +6,35 @@ using UnityEngine;
 namespace GDMachine
 {
 
-	public interface IMachineState
+	public interface IMachineStateTransition
 	{
 
 
+
+	}
+
+
+	public interface IMachineState
+	{
+
+		List<IMachineStateTransition> transitions { get; set; }
+
+		/// <summary>
+		/// Function use for setting up your state
+		/// </summary>
+		void onStateEnter();
+
+		/// <summary>
+		/// Function called for cleanup code
+		/// </summary>
+		void onStateExit();
+
+		/// <summary>
+		/// Called every frame to handle logic
+		/// </summary>
+		void Tick();
+
+		IMachineState checkForTransistion();
 
 	}
 
